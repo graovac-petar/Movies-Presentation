c26c5552db11aace94904472f8efa35889f4952b
graovac-petar <pg20200001@student.fon.bg.ac.rs>
2022-12-05 13:15:57 +0100

    finale2

--- script/forma.js
+++ script/forma.js
@@ -5,35 +5,26 @@ var email = document.getElementsByClassName("e-mail");
 // const zanr = document.getElementsByClassName("zanr");
 
 function prijava() {
-    if (provera()) {
-        alert("Uspesno ste se prijavili");
-        myWindow();
-        window.location.reload();
-    }
-
+  if (provera()) {
+    alert("Uspesno ste se prijavili");
+    // myWindow();
+    window.location.reload();
+  }
 }
 
 dugme1.addEventListener("click", prijava);
 
-
 function provera() {
-
-    if (ime.value === "") {
-        alert("Popunite sva polja!");
-        return false;
-    }
-    else if (prezime.value === "") {
-        alert("Popunite sva polja!");
-        return false;
-    }
-    else if (email.value === "") {
-        alert("Popunite sva polja!");
-        return false;
-    } else {
-        return true;
-    }
-
-
-
+  if (ime.value === "") {
+    alert("Popunite sva polja!");
+    return false;
+  } else if (prezime.value === "") {
+    alert("Popunite sva polja!");
+    return false;
+  } else if (email.value === "") {
+    alert("Popunite sva polja!");
+    return false;
+  } else {
+    return true;
+  }
 }
-
